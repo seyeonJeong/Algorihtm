5e69d1f41cfd1bb37a0ebf66352d9b1861a3a04d
Seyeon <wjdtpdus828@naver.com>
2023-05-20 23:33:21 +0900

    6097

--- python 100/6097.py
+++ python 100/6097.py
@@ -15,10 +15,10 @@ for i in range(n):
     l,d,x,y = map(int,input().split())
     if d == 0:
         for i in range(l):
-            a[x-1][y-1+i] = 1
+            a[x-1][y-1+i] = 1 # x의 값을 그대로 고정 (가로)
     else:
         for i in range(l):
-            a[x-1+i][y-1] = 1
+            a[x-1+i][y-1] = 1 # y의 값을 그대로 고정 (세로)
 
 for i in range(h):
     for j in range(w):
