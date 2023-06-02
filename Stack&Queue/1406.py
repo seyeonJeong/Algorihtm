8d2b9438f13f3c28355fc2adbafd70b8407e4c75
Seyeon <wjdtpdus828@naver.com>
2023-06-02 20:57:16 +0900

    2023/05/31, 1406 ing

--- Stack&Queue/1406.py
+++ Stack&Queue/1406.py
@@ -1,25 +1,24 @@
 import sys
 from collections import deque
 
-str = deque(input())
-
+str = list(input())
+stack_l = []
+#두개의 스택으로 나누는 문제이다. 커서를 따로 구현하지 않고 pop append로 해결해야한다
 n = int(sys.stdin.readline())
-top = len(str)-1
+
 for i in range(n):
     ope = input()
     if ope == 'L':
-        if top != -1:
-            top -= 1
+        if str:
+            stack_l.append(str.pop())
     elif ope == 'D':
-        if top != len(str)-1:
-            top += 1
+        if stack_l:
+            str.append(stack_l.pop())
     elif ope == 'B':
-        if top != -1:
-            del str[top]
-            top -= 1
+        if str:
+            str.pop()
     elif ope[0] == 'P':
-        top += 1
-        str.insert(top,ope[2])
+        str.append(ope[2])
 
 answer = ''.join(s for s in str)
 print(answer)
