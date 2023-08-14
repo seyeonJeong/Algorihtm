bc93ceea851c5f7725aacc4ac31de2b71411b9a7
Seyeon <wjdtpdus828@naver.com>
2023-08-15 00:27:05 +0900

    2023/08/15, 프로그래머스 Hash, 완주하지 못한 선수

---
+++ programmers/Hash/전화번호목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    headHash = 0
+    answer = True
+
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+        if (len(phone_book[i]) < len(phone_book[i + 1])):
+            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
+                answer = False
+                break
+
+    return answer
